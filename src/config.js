778cfc318c8889b626fd43bf1563792ee0606d27
Burl Adkins <adkins1393@gmail.com>
2019-12-17 02:35:33 -0500

    more prep for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://infinite-fortress-70700.herokuapp.com/api',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'get new auth token'
 }
