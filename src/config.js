36d7e9fe13f011375157b17126f933d040058db8
Burl Adkins <adkins1393@gmail.com>
2019-12-18 01:49:50 -0500

    edited config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://infinite-fortress-70700.herokuapp.com/api',
+    API_ENDPOINT: 'https://infinite-fortress-70700.herokuapp.com/api',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'get new auth token'
 }
